fa3f2410913b94c6f21bfd5c2e6f95e7e0786d5d
DELL-WORKSTATIO\ThuyDev <nguyentienthuy94@gmail.com>
2018-12-09 18:06:00 +0700

    fix img card

--- src/app/layout/order/order.component.ts
+++ src/app/layout/order/order.component.ts
@@ -1,12 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
-  styleUrls: ['./order.component.scss']
+  styleUrls: ['./order.component.scss'],
+  encapsulation:ViewEncapsulation.None
 })
 export class OrderComponent implements OnInit {
+  public menuLeft : any [] = [
+    {
+      title : "Tất cả sản phẩm"
+    },
+    {
+      title : "Ngành thủy sản"
+    },
+    {
+      title : "Ngành chăn nuôi"
+    },
+    {
+      title : "Ngành trồng trọt"
+    },
+    {
+      title : "Môi trường"
+    },
+  ]
 
+  public fakeProduct : any [] = [
+    {
+      name : "BiOWISH Aquafarm",
+      img: "assets/images/biowish-3ps-label.jpg",
+      description : "Sản phẩm xử lý môi trường nước được hòa",
+      price: "220.000 vnđ/bao"
+    },
+    {
+      name : "BiOWISH MultiBio 3PS Aquaculture",
+      img: "assets/images/biowish-3ps-label.jpg",
+      description : "Bổ sung hệ vi sinh vật có lợi cho hệ tiêu",
+      price: "170.000 vnđ/bao"
+    },
+    {
+      name : "BiOWISH MultiBio 3PS Livestock",
+      img: "assets/images/biowish-3ps-label.jpg",
+      description : "Sản phẩm xử lý môi trường nước được hòa...",
+      price: "140.000 vnđ/bao"
+    },
+    {
+      name : "BiOWISH Crop 16-40-0",
+      img: "assets/images/biowish-3ps-label.jpg",
+      description : "Sản phẩm xử lý môi trường nước được hòa...",
+      price: "170.000 vnđ/bao"
+    },
+    {
+      name : "BiOWISH Menure & Odor",
+      img: "assets/images/biowish-3ps-label.jpg",
+      description : "Sản phẩm xử lý môi trường nước được hòa...",
+      price: "2.000.000 vnđ/bao"
+    },
+    {
+      name : "BiOWISH MultiBio 3P",
+      img: "assets/images/biowish-3ps-label.jpg",
+      description : "Sản phẩm xử lý môi trường nước được hòa...",
+      price: "230.000 vnđ/bao"
+    },
+  ]
   constructor() { }
 
   ngOnInit() {
