27a2828c0a1169647798910b0c498c4ea11010e1
DELL-WORKSTATIO\ThuyDev <nguyentienthuy94@gmail.com>
2018-12-11 00:44:19 +0700

    thuy
    lam phan regiter ui con button

--- src/app/layout/layout.routing.ts
+++ src/app/layout/layout.routing.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   { path:'', component: LayoutComponent, children: [
           {path: 'category', loadChildren: './category/category.module#CategoryModule' },
           {path: 'order', loadChildren: './order/order.module#OrderModule'},
-          {path: 'shopping-cart', loadChildren: './shopping-cart/shopping-cart.module#ShoppingCartModule'},
+          {path: 'cart', loadChildren: './shopping-cart/shopping-cart.module#ShoppingCartModule'},
           {path: 'order-history', loadChildren: './order-history/order-history.module#OrderHistoryModule'},
           {path: 'register', loadChildren:'./register/register.module#RegisterModule'}    
   ] },
--- src/app/layout/register/register.component.ts
+++ src/app/layout/register/register.component.ts
@@ -7,7 +7,42 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class RegisterComponent implements OnInit {
-
+  public listProvide : any [] = [
+    {
+      name: "Thái Bình"
+    },
+    {
+      name: "Hà Nội"
+    },
+    {
+      name: "Ninh Bình"
+    },
+    {
+      name: "Hải Phòng"
+    },
+  ];
+  public listDistrict : any [] = [
+    {
+      name: 'Quận Hoàng Mai'
+    },
+    {
+      name: 'Quận Nam Từ Liêm'
+    },
+    {
+      name: 'Quận Hai Bà Trưng'
+    },
+    {
+      name: 'Quận Tây Hồ'
+    },
+  ];
+  public listWards : any [] = [
+    {
+      name: 'Phường Phú Đô'
+    },
+    {
+      name: 'Phường Trần Lãm'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
