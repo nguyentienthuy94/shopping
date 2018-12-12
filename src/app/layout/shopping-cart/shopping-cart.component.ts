e2473cd8261ca33c3ac5f980d0e8eeb868c35aad
DELL-WORKSTATIO\ThuyDev <nguyentienthuy94@gmail.com>
2018-12-12 15:38:35 +0700

    thuy
    register shopping cart

--- src/app/layout/shopping-cart/shopping-cart.component.ts
+++ src/app/layout/shopping-cart/shopping-cart.component.ts
@@ -1,12 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.scss']
+  styleUrls: ['./shopping-cart.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ShoppingCartComponent implements OnInit {
-
+  public fakeProduct : any [] = [
+    {
+      name : "BiOWISH Aquafarm",
+      img: "assets/images/biowish-3ps-label.jpg",
+      description : "Sản phẩm xử lý môi trường nước được hòa",
+      price: "220.000 vnđ/bao"
+    },
+    {
+      name : "BiOWISH MultiBio 3PS Aquaculture",
+      img: "assets/images/biowish-3ps-label.jpg",
+      description : "Bổ sung hệ vi sinh vật có lợi cho hệ tiêu...",
+      price: "170.000 vnđ/bao"
+    },
+    {
+      name : "BiOWISH MultiBio 3PS Livestock",
+      img: "assets/images/biowish-3ps-label.jpg",
+      description : "Sản phẩm xử lý môi trường nước được hòa...",
+      price: "140.000 vnđ/bao"
+    },
+    {
+      name : "BiOWISH Crop 16-40-0",
+      img: "assets/images/biowish-3ps-label.jpg",
+      description : "Sản phẩm xử lý môi trường nước được hòa...",
+      price: "170.000 vnđ/bao"
+    },
+    {
+      name : "BiOWISH Menure & Odor",
+      img: "assets/images/biowish-3ps-label.jpg",
+      description : "Sản phẩm xử lý môi trường nước được hòa...",
+      price: "2.000.000 vnđ/bao"
+    },
+    {
+      name : "BiOWISH MultiBio 3P",
+      img: "assets/images/biowish-3ps-label.jpg",
+      description : "Sản phẩm xử lý môi trường nước được hòa...",
+      price: "230.000 vnđ/bao"
+    },
+  ]
   constructor() { }
 
   ngOnInit() {
